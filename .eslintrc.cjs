026011b8f1a48ccd487f76be5c671d45c9185c9f
Sefi Cohen <67965039+seficohen1@users.noreply.github.com>
2023-08-03 17:36:00 +0200

    updating eslint plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,14 +6,16 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime"
   ],
   parserOptions: {
     project: './tsconfig.json',
   },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'prettier'],
+  plugins: ['react-refresh', 'prettier', 'react'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
