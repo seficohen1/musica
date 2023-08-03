933c8e075370113b61902d1cb5bbce12ba463327
Sefi Cohen <67965039+seficohen1@users.noreply.github.com>
2023-08-03 17:15:44 +0200

    first commit + project configuration

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+import './App.css';
+
+import { Grid, Text } from '@chakra-ui/react';
+
+function App() {
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
